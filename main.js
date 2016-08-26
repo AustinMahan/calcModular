1e68735e414caa68573f3ce3b69ec4807773d540
AustinMahan <amahan0424@gmail.com>
2016-08-26 15:23:37 -0600

    final commit

---
+++ main.js
@@ -0,0 +1,9 @@
+var calc = require('./calc.js');
+var add = calc.add
+var sub = calc.sub
+var mult = calc.mult
+var div = calc.div
+console.log(add(1, 2));
+console.log(sub(5,2));
+console.log(mult(2,6));
+console.log(div(6,2));
